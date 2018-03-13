521b9cca5d3bb4efa12d80aea0d2e669e7ac4e29
Abel Quintero <abel.j.quintero@gmail.com>
2018-03-13 17:10:13 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   # your code here
   squared_array = []
   squared_array.each do |numbers|
-    numbers = numbers ** 2
+    numbers == numbers ** 2
     squared_array << numbers
   end
   squared_array
